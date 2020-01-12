6eefbcc718d81b674ca8f8cebc7fd4ac3f302afd
Pontus Olsson <pontus@olsson.me>
2020-01-12 17:19:50 +0100

    Squashed commit of the following:
    commit c459a91a31c07905fab4546a43d9071a1df5ca69
    Author: Pontus Olsson <pontus@olsson.me>
    Date:   Sun Jan 12 17:18:34 2020 +0100
    
        PL-0: poddle can use other tag
    
    commit d4d647c046cb713e979067915732c3c660bb6563
    Author: Pontus Olsson <pontus@olsson.me>
    Date:   Sun Jan 12 17:02:42 2020 +0100
    
        PL-0: remove debug
    
    commit 61c7944df321e029aa14e9160ef76b5f626d3f3f
    Author: Pontus Olsson <pontus@olsson.me>
    Date:   Sun Jan 12 17:00:10 2020 +0100
    
        PL-0: remove extra quotes
    
    commit 195273cb7102cb3e8216c5c293f8bfcd8b4bf2a2
    Author: Pontus Lundgren <pontus@lundgren.in>
    Date:   Sun Jan 12 17:12:36 2020 +0100
    
        PL-0: poddle uses watch-dnsmasq.sh
    
    commit fdac0a772c1ccb8c6d344abb479df7137000e404
    Author: Pontus Olsson <pontus@olsson.me>
    Date:   Sun Jan 12 16:39:17 2020 +0100
    
        PL-0: Add debug flag

--- poddler/poddle.sh
+++ poddler/poddle.sh
@@ -1,14 +1,19 @@
 #!/bin/sh -x
 set -e
 
-container="quay.io/pontuslundgren/shoesmith"
+if [ -z $1 ]
+then tag=latest
+else tag="$1"
+fi
+
+container="quay.io/pontuslundgren/shoesmith:$tag"
 podman="podman"
 #podman="podman --remote-host poddler.local --username root"
-podrun="$podman run -d --rm --pod shoesmith -v shoesmith:/srv/cobbler:z"
+podrun="$podman run -d --rm --pod shoesmith -v shoesmith:/srv/shoesmith:z"
 
 $podman pull $container
 $podman pod create -n shoesmith -p 80:80,443:443,67:67/udp
 $podman volume create shoesmith
 $podrun --name cobbler $container 
 $podrun --name cobbler-proxy $container /usr/sbin/httpd -DFOREGROUND
-$podrun --name cobbler-dhcp $container /usr/sbin/dnsmasq --no-daemon
+$podrun --name cobbler-dhcp $container /watch-dnsmasq.sh
