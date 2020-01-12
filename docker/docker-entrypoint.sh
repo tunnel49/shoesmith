ad642644a05fc33e66fea14aad10b95f2dc8d577
Pontus Olsson <pontus@olsson.me>
2020-01-12 17:32:08 +0100

    PL-0: move tftpboot to shared volume

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 mkdir -p /srv/shoesmith
+mkdir -p /srv/shoesmith/tftpboot
 touch /srv/shoesmith/dnsmasq.conf
 ln -sf /srv/shoesmith/dnsmasq.conf /etc/dnsmasq.conf
 /usr/local/bin/envtpl -o /etc/cobbler/settings /etc/cobbler/settings.j2
