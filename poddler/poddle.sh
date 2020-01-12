59e1c5dd32472d748ca533c2bea9239bdb002a3e
Pontus Lundgren <pontus@lundgren.in>
2020-01-12 09:51:45 +0100

    PL-0: get poddle to work

--- poddler/poddle.sh
+++ poddler/poddle.sh
@@ -2,11 +2,13 @@
 set -e
 
 container="quay.io/pontuslundgren/shoesmith"
-podman="podman --remote-host poddler.local --username root"
-podrun="$podman run -d --rm --pod shoesmith"
+podman="podman"
+#podman="podman --remote-host poddler.local --username root"
+podrun="$podman run -d --rm --pod shoesmith -v shoesmith:/srv/cobbler:z"
 
 $podman pull $container
-$podman pod create -n shoesmith -p 80:80,443:443,10.0.1.146:67:67/udp
+$podman pod create -n shoesmith -p 80:80,443:443,67:67/udp
+$podman volume create shoesmith
 $podrun --name cobbler $container 
 $podrun --name cobbler-proxy $container /usr/sbin/httpd -DFOREGROUND
 $podrun --name cobbler-dhcp $container /usr/sbin/dnsmasq --no-daemon
