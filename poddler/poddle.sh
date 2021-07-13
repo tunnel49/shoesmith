1149c56dd3f9dec64f2ccf789b32bbedd8ef47d2
Pontus Lundgren <pontus@lundgren.in>
2021-07-13 16:31:37 +0200

    Use host networking

--- poddler/poddle.sh
+++ poddler/poddle.sh
@@ -11,10 +11,11 @@ podman="podman"
 #podman="podman --remote-host poddler.local --username root"
 podrun="$podman run -d --rm --pod shoesmith -e HOST_IP=$(hostname -i) -v shoesmith:/srv/shoesmith:z"
 
-brctl hairpin cni-podman0 veth449b401c on
+#brctl hairpin cni-podman0 veth449b401c on
 
 $podman pull $container
-$podman pod create -n shoesmith -p 25151:25151,80:80,443:443,67:67/udp
+$podman pod create -n shoesmith --network=host
+#$podman pod create -n shoesmith -p 25151:25151,80:80,443:443,67:67/udp
 $podman volume create shoesmith
 $podrun --name cobbler $container 
 $podrun --name cobbler-proxy $container /usr/sbin/httpd -DFOREGROUND
