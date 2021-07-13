31b7c00801effaf188b3488b3abe23b1c76ab0f7
Pontus Lundgren (ndf104) <pontus.lundgren@tv4.se>
2021-07-13 17:36:21 +0200

    dont pull

--- poddler/poddle.sh
+++ poddler/poddle.sh
@@ -13,7 +13,7 @@ podrun="$podman run -d --rm --pod shoesmith -e HOST_IP=$(hostname -i) -v shoesmi
 
 #brctl hairpin cni-podman0 veth449b401c on
 
-$podman pull $container
+#$podman pull $container
 $podman pod create -n shoesmith --network=host
 #$podman pod create -n shoesmith -p 25151:25151,80:80,443:443,67:67/udp
 $podman volume create shoesmith
